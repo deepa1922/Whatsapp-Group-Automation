06b385c8fc8f79ad726785bb144b1d6abc42730d
Deepa Saini <113918047+deepa1922@users.noreply.github.com>
2023-08-13 21:38:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+CHROME_PROFILE_PATH: str = "user-data-dir=C:\\Users\\<username>\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
+
+ #config.py
+
+# Set your Chrome profile path below
+#CHROME_PROFILE_PATH = "user-data-dir=CHROME-PROFILE-PATH"
+
+# Replace the above path with your actual Chrome profile path.
+# You can find your Chrome profile path by:
+# 1. Opening Chrome and typing "chrome://version" in the address bar.
+# 2. Look for the "Profile Path" entry in the displayed information.
+# 3. Copy the path and replace "CHROME-PROFILE-PATH" above.
+
+# Important: Keep this path private and don't share it publicly.
+
+# Make sure to adjust the path based on your operating system:
+# - Windows: "C:\\Users\\<username>\\AppData\\Local\\Google\\Chrome\\User Data\\Profile"
+# - macOS: "/Users/<username>/Library/Application Support/Google/Chrome/Profile"
+# - Linux: "/home/<username>/.config/google-chrome/Profile"
